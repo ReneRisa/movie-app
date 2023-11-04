60ffc4d8d935b3db813b45a71ace85da4c1baecf
ReneRisa <risael31@gmail.com>
2023-11-04 13:41:30 -0400

    pass down the favorite movie to the array

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 //import logo from './logo.svg';
 import './App.css';
 import SearchMovies from './SearchMovies';
-
+import { useState } from'react';
 function App() {
   const [favourites, setFavourites] = useState([]); //state of favourites movies in an array
   const addFavourite = (movie) => {
     setFavourites([...favourites, movie]);
   }
+  console.log(favourites);
   return (
     <div className="App">
       <SearchMovies addFavourite={addFavourite}/>
--- src/SearchMovies.jsx
+++ src/SearchMovies.jsx
@@ -1,13 +1,13 @@
 //Search Movies Component
 import { useState } from "react"
-function SearchMovies(){
+function SearchMovies(props){
     const [movieTitle, setMovieTitle] = useState('')
     const searchMovie = async (e) => {
         e.preventDefault()
         try {
             const res = await fetch(`https://www.omdbapi.com/?apikey=a2af4a6&t=${movieTitle}`)
             const data = await res.json()
-            console.log(data)
+            props.addFavourite(data)
         } catch (error) {
             console.log(error)
         }
