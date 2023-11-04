8230cb28194030526f4cdfed4a4f76152000054d
ReneRisa <risael31@gmail.com>
2023-11-04 13:34:20 -0400

    Add states and addFavourites movies function

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 //import logo from './logo.svg';
 import './App.css';
 import SearchMovies from './SearchMovies';
+
 function App() {
+  const [favourites, setFavourites] = useState([]); //state of favourites movies in an array
+  const addFavourite = (movie) => {
+    setFavourites([...favourites, movie]);
+  }
   return (
     <div className="App">
-      <SearchMovies />
+      <SearchMovies addFavourite={addFavourite}/>
     </div>
   );
 }
